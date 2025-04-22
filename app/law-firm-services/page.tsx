baaaff3a49d012f014e97063fa1c7921b9cb4816
John Uchendu <johnchnd195@gmail.com>
2025-04-22 18:16:09 +0100

    changes

--- app/law-firm-services/page.tsx
+++ app/law-firm-services/page.tsx
@@ -2,6 +2,36 @@ import OurServices from '@/components/Services'
 import Testimonials from '@/components/Testimonial'
 import React from 'react'
 
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'en-GB': '/en-GB',
+    },
+  },
+  // openGraph: {
+  //   images: '/og-image.png',
+  // },
+  title: "Law Firm Services",
+  description: "Number 1 law firm services with K.K. Ubani & Co.",
+  generator: '',
+  applicationName: 'K.K.Ubani&Co.',
+  referrer: 'origin-when-cross-origin',
+  keywords: ["K.K. Ubani & Co.", "Law firm services", "Corporate", "Commercial", "Energy and Natural Resources"],
+  authors: [{ name: "John"}   ],
+  creator: "John Uchendu",
+  publisher: "John Uchendu",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+};
+
 const pages = () => {
   return (
     <div>
