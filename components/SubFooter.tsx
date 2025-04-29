6f17b330241ec9c4a15cb03dff2ea2d2dda472e8
John Uchendu <johnchnd195@gmail.com>
2025-04-29 23:59:15 +0100

    changes

--- components/SubFooter.tsx
+++ components/SubFooter.tsx
@@ -1,10 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const SubFooter = () => {
-    return (
+  return (
+    <>
       <div className="bg-gray-900 text-white text-center py-4 text-sm">
         Copyright © 2025 K.K. Ubani & Co. All Rights Reserved.
       </div>
-    );
-  };
-  
-  export default SubFooter;
-  
+
+      <div className="mt-12 flex justify-center">
+        <Link href="https://eaglecontractoragency.vercel.app/">
+          <Image
+            src="/eagle.png"
+            alt="Eagle Contractor Logo"
+            width={35}
+            height={35}
+            className="object-contain hover:opacity-80 transition duration-300"
+          />ECA
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default SubFooter;
